6eeea765f828fddf9a6a6e9d09ecb8c98cb1c52e
Nyllereth Jimenez <nyllerethj@gmail.com>
2025-09-30 10:55:22 -0500

    se agragaron mas tarjetas a la vista welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,11 @@
 @extends('adminlte::page')
-@section('title', $title ?? 'Dashboard')
+@section('title', $title ?? '  Pagina principal')
 
 
 
 @section('content')
-    <div class="content-wrapper">
+
+        <div class="content-wrapper">
   <!-- Encabezado -->
   <section class="content-header">
     <div class="container-fluid">
@@ -16,14 +17,14 @@
   <!-- Contenido principal -->
   <section class="content-">
     <div class="container-fluid">
-      <div class="row justify-content-center">
+      <div class="row ">
 
         <!-- Tarjeta: Mesas disponibles -->
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-3 col-4">
           <div class="small-box bg-success">
             <div class="inner">
               <h3>12</h3>
-              <p>Mesas Disponibles</p>
+              <p>Mesas </p>
             </div>
             <div class="icon">
               <i class="fas fa-table"></i>
@@ -33,7 +34,7 @@
         </div>
 
         <!-- Tarjeta: Productos -->
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-3 col-4">
           <div class="small-box bg-info">
             <div class="inner">
               <h3>35</h3>
@@ -47,11 +48,11 @@
         </div>
 
         <!-- Tarjeta: Partidas activas -->
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-3 col-4">
           <div class="small-box bg-warning">
             <div class="inner">
               <h3>5</h3>
-              <p>Partidas Activas</p>
+              <p>Torneos</p>
             </div>
             <div class="icon">
               <i class="fas fa-play-circle"></i>
@@ -61,11 +62,11 @@
         </div>
 
         <!-- Tarjeta: Ingresos de hoy -->
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-3 col-4">
           <div class="small-box bg-danger">
             <div class="inner">
               <h3>$450</h3>
-              <p>Ingresos Hoy</p>
+              <p>Ingresos</p>
             </div>
             <div class="icon">
               <i class="fas fa-dollar-sign"></i>
@@ -73,6 +74,38 @@
             <a href="#" class="small-box-footer">Ver reportes <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
+
+         <!--TARJETA: Pagos -->
+        <div class="col-lg-3 col-4">
+          <div class="small-box bg-primary">
+            <div class="inner">
+              <h3>18</h3>
+              <p>Pagos</p>
+            </div>
+            <div class="icon">
+              <i class="fas fa-credit-card"></i>
+            </div>
+            <a href="#" class="small-box-footer">Ver pagos <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <!-- Tarjeta: informes -->
+      <div class="col-lg-3 col-6">
+          <div class="small-box bg-secondary">
+            <div class="inner">
+              <h3>7</h3>
+              <p>Informes</p>
+            </div>
+            <div class="icon">
+              <i class="fas fa-chart-line"></i>
+            </div>
+            <a href="#" class="small-box-footer">Ver informes <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+
+
+
 
       </div>
 
@@ -92,6 +125,7 @@
 
 @section('js')
     @stack('scripts')
+
 @stop
 
 
