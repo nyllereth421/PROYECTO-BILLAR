f6c0401057fee7e2da1dbbc5394ad139287f80cd
Nyllereth Jimenez <nyllerethj@gmail.com>
2025-09-30 11:23:28 -0500

    se creo el formulario de la vista create de productos

---
+++ resources/views/productos/create.blade.php
@@ -0,0 +1,46 @@
+@extends('adminlte::page')
+
+@section('title', 'Crear Producto')
+
+@section('content')
+<div class="container">
+    <h1>Crear Producto</h1>
+
+    @if ($errors->any())
+      <div class="alert alert-danger">
+          <ul>
+              @foreach($errors->all() as $error)
+                  <li>{{ $error }}</li>
+              @endforeach
+          </ul>
+      </div>
+    @endif
+
+    <form action="{{ route('productos.store') }}" method="POST">
+        @csrf
+
+        <div class="form-group">
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" id="nombre" class="form-control" value="{{ old('nombre') }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="descripcion">Descripción</label>
+            <textarea name="descripcion" id="descripcion" class="form-control">{{ old('descripcion') }}</textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="precio">Precio</label>
+            <input type="number" step="0.01" name="precio" id="precio" class="form-control" value="{{ old('precio') }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="stock">Stock</label>
+            <input type="number" name="stock" id="stock" class="form-control" value="{{ old('stock') }}" required>
+        </div>
+
+        <button type="submit" class="btn btn-success mt-3">Guardar</button>
+        <a href="{{ route('productos.index') }}" class="btn btn-secondary mt-3">Cancelar</a>
+    </form>
+</div>
+@stop
