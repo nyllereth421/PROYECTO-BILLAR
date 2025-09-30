298e1a911c0ae1fced075295d9b4ba980c507688
Anderson28062003 <roacamilo377@gmail.com>
2025-09-30 11:08:50 -0500

    cambios en el welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,97 +1,101 @@
 @extends('adminlte::page')
+
 @section('title', $title ?? 'Dashboard')
 
-
-
 @section('content')
-    <div class="content-wrapper">
-  <!-- Encabezado -->
-  <section class="content-header">
-    <div class="container-fluid">
-      <h1 class="text-center">Bienvenido a Billar Nexus</h1>
-      <p class="text-center">Panel de gestión del sistema</p>
-    </div>
-  </section>
-
-  <!-- Contenido principal -->
-  <section class="content-">
-    <div class="container-fluid">
-      <div class="row justify-content-center">
-
-        <!-- Tarjeta: Mesas disponibles -->
-        <div class="col-lg-3 col-6">
-          <div class="small-box bg-success">
-            <div class="inner">
-              <h3>12</h3>
-              <p>Mesas Disponibles</p>
+<div class="d-flex align-items-center" 
+     style="margin: -1.5rem; min-height: calc(100vh - 3.5rem - 1px); background: url({{ asset('vendor/adminlte/dist/img/fondo.png') }}) no-repeat center center fixed; background-size cover;">
+    <section class="content mx-auto" style="max-width: 1140px; width: 100%; background-color: rgba(0,0,0,0.6); border-radius: 10px; padding: 20px;">
+        <div class="container text-center text-white">
+            <!-- Encabezado -->
+            <div class="mb-4">
+                <h1>Bienvenido a Billar Nexus</h1>
+                <p>Panel de gestión del sistema</p>
             </div>
-            <div class="icon">
-              <i class="fas fa-table"></i>
+
+            <!-- Contenedor con sistema de columnas -->
+            <div class="row justify-content-center">
+
+                <!-- Tarjeta: Mesas disponibles -->
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>12</h3>
+                            <p>Mesas</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-table"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">Ver más <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <!-- Tarjeta: Productos -->
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>35</h3>
+                            <p>Productos</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-cocktail"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">Ver productos <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <!-- Tarjeta: Torneos -->
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>5</h3>
+                            <p>Torneos</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-play-circle"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">Ver torneos <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <!-- Tarjeta: Ingresos -->
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>$450</h3>
+                            <p>Ingresos</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-dollar-sign"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">Ver reportes <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <!-- Tarjeta: Pago -->
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="small-box bg-primary">
+                        <div class="inner">
+                            <h3>Pagos</h3>
+                            <p>pagos</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-credit-card"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">Ver pagos <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
             </div>
-            <a href="#" class="small-box-footer">Ver más <i class="fas fa-arrow-circle-right"></i></a>
-          </div>
         </div>
-
-        <!-- Tarjeta: Productos -->
-        <div class="col-lg-3 col-6">
-          <div class="small-box bg-info">
-            <div class="inner">
-              <h3>35</h3>
-              <p>Productos</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-cocktail"></i>
-            </div>
-            <a href="3" class="small-box-footer">Ver productos <i class="fas fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-
-        <!-- Tarjeta: Partidas activas -->
-        <div class="col-lg-3 col-6">
-          <div class="small-box bg-warning">
-            <div class="inner">
-              <h3>5</h3>
-              <p>Partidas Activas</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-play-circle"></i>
-            </div>
-            <a href="#" class="small-box-footer">Ver partidas <i class="fas fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-
-        <!-- Tarjeta: Ingresos de hoy -->
-        <div class="col-lg-3 col-6">
-          <div class="small-box bg-danger">
-            <div class="inner">
-              <h3>$450</h3>
-              <p>Ingresos Hoy</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-dollar-sign"></i>
-            </div>
-            <a href="#" class="small-box-footer">Ver reportes <i class="fas fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-
-      </div>
-
-      <!-- Más contenido si deseas -->
-      <!-- Por ejemplo: últimos movimientos, estadísticas, etc -->
-
-    </div>
-  </section>
+    </section>
 </div>
-
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/admin-custom.css') }}">
     @stack('styles')
 @stop
 
 @section('js')
     @stack('scripts')
 @stop
-
-
