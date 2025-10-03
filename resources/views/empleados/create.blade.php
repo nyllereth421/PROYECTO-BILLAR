c0451a42e8c0e9d114d34fe9408657505db5fb3e
Anderson28062003 <roacamilo377@gmail.com>
2025-10-03 07:57:02 -0500

    vista create

--- resources/views/empleados/create.blade.php
+++ resources/views/empleados/create.blade.php
@@ -20,8 +20,8 @@
         @csrf
 
          <div class="form-group">
-            <label for="idproducto">id Producto</label>
-            <input type="integer" name="idproducto" id="idproducto" class="form-control" placeholder="Ingrese el ID del producto" required>
+            <label for="numerodocumento">Número de Documento</label>
+            <input type="integer" name="numerodocumento" id="numerodocumento" class="form-control" placeholder="Ingrese el número de documento del empleado" required>
         </div>
 
 
@@ -31,18 +31,58 @@
         </div>
 
         <div class="form-group">
-            <label for="descripcion">Descripción</label>
-            <textarea name="descripcion" id="descripcion" class="form-control" placeholder="Ingrese la descripción del producto"></textarea>
+            <label for="cargo">Cargo</label>
+            <textarea name="cargo" id="cargo" class="form-control" placeholder="Ingrese el cargo del empleado"></textarea>
         </div>
 
         <div class="form-group">
-            <label for="precio">Precio</label>
-            <input type="number" step="0.01" name="precio" id="precio" class="form-control" placeholder="Ingrese el precio del producto" required>
+            <label for="salario">salario</label>
+            <input type="number" step="0.01" name="salario" id="salario" class="form-control" placeholder="Ingrese el salario del producto" required>
         </div>
 
         <div class="form-group">
-            <label for="stock">Stock</label>
-            <input type="number" name="stock" id="stock" class="form-control" placeholder="Ingrese el stock del producto" required>
+            <label for="estado">estado</label>
+            <input type="number" name="estado" id="estado" class="form-control" placeholder="Ingrese el estado del producto" required>
+        </div>
+
+         <div class="form-group">
+            <label for="tipodocumento">Tipo de Documento</label>
+            <input type="number" name="tipodocumento" id="tipodocumento" class="form-control" placeholder="Ingrese el tipo de documento del empleado" required>
+        </div>
+
+         <div class="form-group">
+            <label for="apellidos">Apellidos</label>
+            <input type="text" name="apellidos" id="apellidos" class="form-control" placeholder="Ingrese los apellidos del empleado" required>
+        </div>
+
+         <div class="form-group">
+            <label for="estado">estado</label>
+            <input type="number" name="estado" id="estado" class="form-control" placeholder="Ingrese el estado del producto" required>
+        </div>
+
+         <div class="form-group">
+            <label for="estado">estado</label>
+            <input type="number" name="estado" id="estado" class="form-control" placeholder="Ingrese el estado del producto" required>
+        </div>
+
+         <div class="form-group">
+            <label for="estado">estado</label>
+            <input type="number" name="estado" id="estado" class="form-control" placeholder="Ingrese el estado del producto" required>
+        </div>
+
+         <div class="form-group">
+            <label for="estado">estado</label>
+            <input type="number" name="estado" id="estado" class="form-control" placeholder="Ingrese el estado del producto" required>
+        </div>
+
+         <div class="form-group">
+            <label for="estado">estado</label>
+            <input type="number" name="estado" id="estado" class="form-control" placeholder="Ingrese el estado del producto" required>
+        </div>
+
+         <div class="form-group">
+            <label for="estado">estado</label>
+            <input type="number" name="estado" id="estado" class="form-control" placeholder="Ingrese el estado del producto" required>
         </div>
 
         <button type="submit" class="btn btn-success mt-3">Guardar</button>
